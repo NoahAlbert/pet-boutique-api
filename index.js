7d6f26ee351c58d00087a9613cc41aadbaf923cf
NoahAlbert <no.albert113@gmail.com>
2021-07-12 17:03:58 -0400

    added a function to create a customer from the body

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const cors = require('cors')
-const { getCustomers, getCustomerByID, getCustomerByQuery } = require('./src/customers')
+const { getCustomers, getCustomerByID, getCustomerByQuery, createCustomer } = require('./src/customers')
 
 const app = express()
 app.use(cors())
+app.use(express.json())
 
 app.get('/customers/search', getCustomerByQuery)
 app.get('/customers/:id', getCustomerByID)
 app.get('/customers', getCustomers)
 
-//app.post('/customers', createCustomer)
+app.post('/customers', createCustomer)
 
 
 app.listen(3000, () => console.log('listening to 3000'))
--- src/customers.js
+++ src/customers.js
@@ -1,60 +1,72 @@
 const admin = require('firebase-admin')
 const creds = require('../credentials.json')
 
-
-function connectDB(){
-    if(!admin.apps.length){
-        admin.initializeApp({
-          credential: admin.credential.cert(creds)
-        })
-    }
-    return admin.firestore()
+function connectDB() {
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(creds),
+    })
+  }
+  return admin.firestore()
 }
 
 exports.getCustomers = (req, res) => {
-    const db = connectDB()
-    db.collection('customers').get()
+  const db = connectDB()
+  db.collection('customers')
+    .get()
     .then(customerCollection => {
-        const allCustomers = customerCollection.docs.map(doc => doc.data())
-        res.send(allCustomers)
+      const allCustomers = customerCollection.docs.map(doc => doc.data())
+      res.send(allCustomers)
     })
     .catch(err => {
-        console.error(err)
-        res.status(500).send(err)
+      console.error(err)
+      res.status(500).send(err)
     })
-
 }
 
 exports.getCustomerByID = (req, res) => {
-    if(!req.params.id){
-        res.status(400).send('No customer found')
-        return
-    }
-    const db = connectDB()
-    db.collection('customers').doc(req.params.id).get()
-    .then( doc => {
-        const customer = doc.data()
-        customer.id = doc.id
-        res.send(customer)
+  if (!req.params.id) {
+    res.status(400).send('No customer found')
+    return
+  }
+  const db = connectDB()
+  db.collection('customers')
+    .doc(req.params.id)
+    .get()
+    .then(doc => {
+      const customer = doc.data()
+      customer.id = doc.id
+      res.send(customer)
     })
     .catch(err => {
-        console.error(err)
-        res.status(500).send(err)
+      console.error(err)
+      res.status(500).send(err)
     })
 }
 
 exports.getCustomerByQuery = (req, res) => {
-    const { fname } = req.query
-    const db = connectDB()
-    db.collection('customers').where('firstName', '==', fname).get()
-    .then( customerCollection => {
-        const matches = customerCollection.docs.map(doc => {
-            let customer = doc.data()
-            customer.id = doc.id
-            return customer
-        })
-        res.send(matches)
+  const { fname } = req.query
+  const db = connectDB()
+  db.collection('customers')
+    .where('firstName', '==', fname)
+    .get()
+    .then(customerCollection => {
+      const matches = customerCollection.docs.map(doc => {
+        let customer = doc.data()
+        customer.id = doc.id
+        return customer
+      })
+      res.send(matches)
     })
     .catch(err => res.status(500).send(err))
+}
 
+exports.createCustomer = (req, res) => {
+  const db = connectDB()
+  db.collection('customers')
+    .add(req.body)
+    .then(docRef => {
+      res.send(docRef.id)
+    })
+    .catch(err => res.status(500).send('Customer could not be created:', err))
 }
